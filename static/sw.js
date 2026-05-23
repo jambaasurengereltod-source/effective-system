d9c623b1ce7860b7a5bef0e35c19a600afcff30d
Tanii Ner <taniih@example.com>
2026-05-23 12:33:49 +0800

    pwa mobile app fully completed via notepad

---
+++ static/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Апп онлайн ажиллах тул fetch-ийг зүгээр өнгөрөөнө
+});
